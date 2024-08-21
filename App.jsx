4132b00351a6497727e1c51df03952504ff3b0b3
Irwing <86335744+Irwingduran@users.noreply.github.com>
2024-08-21 09:53:01 -0600

    development commit

---
+++ App.jsx
@@ -0,0 +1,40 @@
+
+import './App.css'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+import Services from './components/Services'
+import About from './components/About'
+import Address from './components/Address'
+import Blog from './components/Blog'
+import Banner from './components/Banner'
+import MyFooter from './components/MyFooter'
+import Home from "./components/Home"
+import Gallery from './components/Galllery'
+import Parners from './components/Parners'
+import LandingPage from './components/LandingPage'
+
+
+function App() {
+
+
+  return (
+    
+    <>
+      <Home/>
+      <Navbar />
+      <Hero/>
+      <Services />
+      <About />
+      <Address />
+      <Gallery/>
+      <Blog />
+      <Parners />
+      <Banner />
+      <MyFooter />
+     <LandingPage />
+      
+    </>
+  )
+}
+
+export default App
