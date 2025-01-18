f79e46461af9052eaef632990a79c2bb31750849
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-01-18 15:26:01 -0600

    update Navbar.tsx

--- src/components/en/Navbar.tsx
+++ src/components/en/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { FaXmark, FaBars } from "react-icons/fa6";
 import { useLocation, useNavigate } from 'react-router-dom';
+import ScrollToHash from "../../utils/ScrollToHash";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
