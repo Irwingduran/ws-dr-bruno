1a63c586f7368fe6751b3df4223eef0dc2f05f94
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-02-20 09:38:53 -0600

    Update Modal.tsx

--- src/components/en/home/Modal.tsx
+++ src/components/en/home/Modal.tsx
@@ -16,15 +16,33 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, description, imag
       <div className="bg-white rounded-lg shadow-lg w-11/12 max-w-3xl p-6 relative">
         {/* Close Button */}
         <button
-          className="absolute top-4 right-4 text-gray-500 hover:text-gray-700 z-30"
+          className="absolute top-3 right-3 text-gray-500 hover:text-gray-700 z-30 focus:outline-none"
           onClick={onClose}
         >
-         Close
+          <span className="sr-only">Close</span>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M6 18L18 6M6 6l12 12"
+            ></path>
+          </svg>
         </button>
 
         {/* Content */}
         <h2 className="text-2xl font-bold text-neutralDGrey mb-2">{title}</h2>
-        <img src={imageSrc} alt={title} className="w-full h-full object-cover rounded-lg mb-4" />
+        <img
+          src={imageSrc}
+          alt={title}
+          className="w-full h-auto object-cover rounded-lg mb-4"
+        />
         <p className="text-neutralDGrey">{description}</p>
       </div>
     </div>
