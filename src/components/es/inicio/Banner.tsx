292b67276760c0f16f71b95369e8f0d56a38780b
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-02-24 11:38:30 -0600

    Update Banner.tsx

--- src/components/es/inicio/Banner.tsx
+++ src/components/es/inicio/Banner.tsx
@@ -1,5 +1,3 @@
-import { Link } from 'react-scroll';
-
 const Banner = () => {
   return (
     <div className='px-4 lg:px-14 max-w-screen-2x1 my-10 mx-auto bg-neutralSilver py-16'>
@@ -9,17 +7,17 @@ const Banner = () => {
           ¿Eres candidato para cirugía de próstata?
           </h2>
           <div className='flex items-center justify-center gap-8'>
-            <Link
-              to="https://api.whatsapp.com/send/?phone=5212228457430&text=Hola+Dr.+encontr%C3%A9+su+perfil+en+findoctor+y+me+gustar%C3%ADa+realizar+una+consulta&type=phone_number&app_absent=0"
-              smooth={true}
-              duration={500}
+            <a
+              href="https://api.whatsapp.com/send/?phone=5212228457430&text=Hola+Dr.+encontr%C3%A9+su+perfil+en+findoctor+y+me+gustar%C3%ADa+realizar+una+consulta&type=phone_number&app_absent=0"
               className='btn-primary text-white cursor-pointer'
+              target="_blank"
+              rel="noopener noreferrer"
             >
              Agenda una cita aquí
               <svg xmlns='http://www.w3.org/2000/svg' width='14' height='8' viewBox='0 0 14 8' fill='none' className='inline-block ml-2'>
                 <path d='M10.2503 7.00012L12.7201 4.53039C13.013 4.23749 13.013 3.7626 12.7201 3.4697L10.2503 0.999966M12.5004 4.00004L1.50012 4.00004' stroke='white' />
               </svg>
-            </Link>
+            </a>
           </div>
         </div>
       </div>
