0c78ec6da513a87a9001b2db03fd74a015963742
Irwingduran <86335744+Irwingduran@users.noreply.github.com>
2025-05-12 13:17:06 -0600

    add meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
 import { Suspense } from "react";
 import { AppRouter } from "./Router";
+import { HelmetProvider } from 'react-helmet-async';
+import MetaTags from './MetaTags';
 
 function App() {
   return (
-    <Router>
-      <Suspense>
-        <AppRouter/>
-      </Suspense>
-    </Router>
+    <HelmetProvider>
+      <MetaTags />
+      <Router>
+        <Suspense>
+          <AppRouter/>
+        </Suspense>
+      </Router>
+    </HelmetProvider>
   )
 }
 
